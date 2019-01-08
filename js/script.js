9b7f45f1e549d7b0612df070fc1d3c47eda0acab
Andrei Ionică <andrei.ionica97@gmail.com>
2019-01-08 04:32:54 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-//script
+function toggleMenu() {
+    var x = document.getElementById("nav");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
